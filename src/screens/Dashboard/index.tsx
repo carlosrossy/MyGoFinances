1308095b1b5a53d91b7e66793473bd1286dd5740
CarlosBitizin <carlospintorossy07@gmail.com>
2022-07-12 10:28:11 -0400

    Header component creation

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -2,15 +2,34 @@ import React from 'react';
 
 import {
     Container,
-    Title,
-
+    Header,
+    UserContainer,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
 } from './styles'
 
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserContainer>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/82986679?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Carlos</UserName>
+                        </User>
+                    </UserInfo>
+                </UserContainer>
+
+
+            </Header>
         </Container>
     )
 }
